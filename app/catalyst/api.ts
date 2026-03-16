c345b79ef8827ebc0f5015dd7f98e88713879489
Jy Wen <jiayangwen622@gmail.com>
2026-03-15 21:43:46 -0400

    feat catalyst frontend direct api url support

--- app/catalyst/api.ts
+++ app/catalyst/api.ts
@@ -1,2 +1,5 @@
 import axios from 'axios';
-export const catalystApi = axios.create({ baseURL: '/catalyst-api' });
+
+const baseURL = process.env.NEXT_PUBLIC_CATALYST_API_URL || '/catalyst-api';
+
+export const catalystApi = axios.create({ baseURL });
